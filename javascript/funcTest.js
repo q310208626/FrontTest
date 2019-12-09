dd6574709a659a45d0a1d42414546c22a4cb18ee
q310208626 <15521385907@163.com>
2019-12-09 17:52:20 +0800

    闭包

--- javascript/funcTest.js
+++ javascript/funcTest.js
@@ -1,14 +1,59 @@
-function aFunc(){
-    return this;
+// this 测试
+// function aFunc(){
+//     return this;
+// }
+// console.log(aFunc());   // windows
+
+
+// var person = {
+//     name:"lin",
+//     printPerson:function(){
+//         return this.name;
+//     }
+// }
+
+// console.log(person.printPerson());
+
+// // arguments 练习
+// function sum(){
+//     let temp =0;
+//     if(arguments.length >0){
+//         for (let index = 0; index < arguments.length; index++) {
+//             const element = arguments[index];
+//             temp += element;
+//         }
+//     }
+//     console.log(arguments.callee);
+//     return temp;
+// }
+// console.log(sum(1,3,7,10,true));
+
+// 闭包测试
+var funcArr = [
+    {func:function(){}},
+    {func:function(){}},
+    {func:function(){}}
+]
+
+function showTextFunc(text){
+    console.log(text);
 }
-console.log(aFunc());   // windows
 
 
-var person = {
-    name:"lin",
-    printPerson:function(){
-        return this.name;
+
+function setOnClick(){
+    var showText = ["111","222","333"];
+    
+    for( var i = 0; i < 3; i ++){
+        var text = showText[i];
+        funcArr[i].func = (function(){ 
+            return function(){showTextFunc(text);}
+        })();
     }
 }
 
-console.log(person.printPerson());
+setOnClick();
+
+funcArr[0].func();
+funcArr[1].func();
+funcArr[2].func();
